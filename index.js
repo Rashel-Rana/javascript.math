02600357a190865295cc6c4948deaacdadc23e24
Rashel Rana <rashel24.bd@gmail.com>
2024-07-26 21:18:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+document.getElementById("demo").innerHTML = 10;
+let x = 5;
+let y = 6;
+let z = 8;
+let a = x+y+z;
+document.getElementById ("demo").innerHTML = a;
+
+// math function in javascript 
+{
+  document.getElementById ("demo").innerHTML = Math.E;
+  document.getElementById ("demo").innerHTML = Math.LN10;
+  document.getElementById ("demo").innerHTML = Math.PI;
+  document.getElementById ("demo").innerHTML = Math.abs(3.14159);
+  document.getElementById ("demo").innerHTML = Math.floor(3.14159);
+  document.getElementById ("demo").innerHTML = Math.round(3.14159);
+  document.getElementById ("demo").innerHTML = Math.max(3.14159 ,  5);
+  document.getElementById ("demo").innerHTML = Math.sqrt(3.14159);
+  document.getElementById ("demo").innerHTML = Math.tan(3.14159);
+  document.getElementById ("demo").innerHTML = Math.random(3.14159);
+}
+// javascript date and  time
+{
+  document.getElementById ("demo").innerHTML = Date();
+  document.getElementById ("demo").innerHTML = Time();
+  document.getElementById ("demo").innerHTML = date.getFullYear();
+
+
+
+}
+{
+  let a = 3.1416;
+  let r = 4.55;
+  let area = a*r**2;
+  document.getElementById("nemo").innerHTML = 5+6;
+
+
+}
